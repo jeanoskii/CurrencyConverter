445e8351f68bcf3d4742726e348957b40af6c768
Jeano Frederick Ermitaño <jeano.ermitano@laspinas.sti.edu.ph>
2022-01-06 12:06:31 +0800

    initial commit

---
+++ FinalsProject/FinalsProject/TabPage1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FinalsProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabPage1 : ContentPage
+    {
+        private int count = 0;
+        public TabPage1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            lblClick.Text = "You clicked " + count.ToString() + " times.";
+        }
+    }
+}
