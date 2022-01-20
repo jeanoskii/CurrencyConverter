e0e7919f2612fb0ff4491b36ccb2e7a878820e96
Jeano Frederick Ermitaño <jeano.ermitano@laspinas.sti.edu.ph>
2022-01-20 12:09:05 +0800

    added currency convert function

---
+++ FinalsProject/FinalsProject/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalsProject
+{
+    class Currency
+    {
+        public double amount { get; set; }
+        public string @base { get; set; }
+        public string date { get; set; }
+        public Dictionary<string,string> rates { get; set; }
+
+    }
+}
--- FinalsProject/FinalsProject/TabPage1.xaml.cs
+++ FinalsProject/FinalsProject/TabPage1.xaml.cs
@@ -21,13 +21,41 @@ namespace FinalsProject
             InitializeComponent();
             //TestConnection();
             GetCurrencies();
+            //ConvertCurrency(1.0, "USD", "PHP");
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            ConvertCurrency(double.Parse(ConvertAmountFrom.Text), ConvertCurrencyFrom.SelectedItem.ToString(), ConvertCurrencyTo.SelectedItem.ToString());
+            // Task<double> convertedAmount = ConvertCurrency(double.Parse(ConvertAmountFrom.Text),
+            //    ConvertCurrencyFrom.SelectedItem.ToString(),
+            //    ConvertCurrencyTo.SelectedItem.ToString());
+            //ConvertAmountTo.Text = convertedAmount.Result.ToString();
         }
 
+        private async void ConvertCurrency(double AmountFrom, string CurrencyFrom, string CurrencyTo)
+        {
+            double amountTo = 0.0;
 
+            // call the api
+            // supply the parameters
+            string requestUri = endpoint +
+                "latest?amount=" + AmountFrom +
+                "&from=" + CurrencyFrom +
+                "&to=" + CurrencyTo;
+            string response = await httpClient.GetStringAsync(requestUri);
+            
+            Currency formattedResponse;
+            formattedResponse = JsonConvert.DeserializeObject<Currency>(response);
+            //ConvertAmountTo.Text = formattedResponse.date;
+            Dictionary<string, string> rates = formattedResponse.rates;
+            string amountString;
+            rates.TryGetValue(CurrencyTo, out amountString);
+            //amountTo = double.Parse(amountString);
+
+
+            ConvertAmountTo.Text = amountString;
+        }
 
         private async void GetCurrencies()
         {
